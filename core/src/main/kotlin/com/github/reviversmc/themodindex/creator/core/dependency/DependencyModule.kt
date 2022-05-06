3543b1eb3baab9e1a30156e861f172718b9d42b5
Cloudate9 <thealtfulthing@tutanota.com>
2022-05-05 22:15:19 -0700

    Teach Koin how to create an ApiDownloader

--- core/src/main/kotlin/com/github/reviversmc/themodindex/creator/core/dependency/DependencyModule.kt
+++ core/src/main/kotlin/com/github/reviversmc/themodindex/creator/core/dependency/DependencyModule.kt
@@ -1,8 +1,11 @@
 package com.github.reviversmc.themodindex.creator.core.dependency
 
+import com.github.reviversmc.themodindex.api.downloader.ApiDownloader
+import com.github.reviversmc.themodindex.api.downloader.DefaultApiDownloader
 import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dependencyModule = module {
@@ -12,5 +15,6 @@ val dependencyModule = module {
             prettyPrint = true
         }
     }
+    factory { DefaultApiDownloader(get()) } bind ApiDownloader::class
     singleOf(::OkHttpClient)
 }
