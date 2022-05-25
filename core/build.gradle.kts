9ca0b9721648bd39de321dbbed771dc8ce5b6731
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-05-25 12:37:30 -0700

    API bump and add okhttp (since the-mod-index-api no longer bundles it)

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 dependencies {
-    api("com.github.reviversmc:the-mod-index-api:5.0.0")
+    api("com.github.reviversmc:the-mod-index-api:5.1.0")
+    api("com.squareup.okhttp3:okhttp:4.9.3")
     api("org.kohsuke:github-api:1.306")
 }
