729976398d0d024bea9973c65f2c62672eda0da1
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-05-29 23:43:09 -0700

    Update test

--- core/src/test/kotlin/ApiCallTest.kt
+++ core/src/test/kotlin/ApiCallTest.kt
@@ -1,38 +1,35 @@
 import com.github.reviversmc.themodindex.creator.core.apicalls.*
-import com.github.reviversmc.themodindex.creator.core.dependency.dependencyModule
-import kotlinx.serialization.SerializationException
+import kotlinx.serialization.ExperimentalSerializationApi
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.RegisterExtension
-import org.koin.core.Koin
-import org.koin.core.parameter.parametersOf
 import org.koin.test.KoinTest
 import org.koin.test.inject
 import org.koin.test.junit5.KoinTestExtension
+import java.io.IOException
 import java.util.Properties
 import kotlin.test.*
 
 class ApiCallTest : KoinTest {
 
+    @ExperimentalSerializationApi
     @JvmField
     @RegisterExtension
     val koinTestExtension = KoinTestExtension.create {
         modules( //We be testing the default api call classes, therefore no custom module needed
-            apiCallModule, dependencyModule
+            apiCallModule
         )
     }
 
     /**
-     * Uses Sodium by jellysquid to test the API call.
+     * Uses Modget by ReviversMC to test the API call.
      *
      * @author ReviversMC
      * @since 1.0.0
      */
     @Test
-    fun `curse api test`(koin: Koin) {
-        assertThrows<SerializationException> {
-            (koin.get<CurseForgeApiCall> { parametersOf("broken-api-key") }).mod(394468)
-        }
+    fun `curse api test`() {
+        val curseForgeApiCall by inject<CurseForgeApiCall>()
+        assertNull(curseForgeApiCall.mod("broken-api-key", 533960).execute().body())
 
         fun curseApiKey(): String {
             val properties = Properties()
@@ -40,50 +37,45 @@ class ApiCallTest : KoinTest {
                 this.javaClass.getResourceAsStream("/curseForgeApiKey.properties")
                     ?: return System.getenv("CURSEFORGE_API_KEY") ?: "broken-api-key"
             )
-            return properties.getProperty("api_key") ?: System.getenv()["CURSEFORGE_API_KEY"]
-            ?: "broken-api-key"
+            return properties.getProperty("api_key") ?: System.getenv()["CURSEFORGE_API_KEY"] ?: "broken-api-key"
         }
 
-        val curseForgeApiCall by inject<CurseForgeApiCall> {
-            parametersOf(curseApiKey())
-        }
-
-        val curseForgeMod = curseForgeApiCall.mod(394468)!!.data
+        val curseForgeMod = curseForgeApiCall.mod(curseApiKey(), 533960).execute().body()!!.data
         assertNotNull(curseForgeMod)
-        assertEquals("sodium", curseForgeMod.name.lowercase())
-        assertEquals("https://github.com/caffeinemc/sodium-fabric", curseForgeMod.links.sourceUrl.lowercase())
+        assertEquals("modget", curseForgeMod.name.lowercase())
+        assertEquals("https://github.com/reviversmc/modget", curseForgeMod.links.sourceUrl.lowercase())
         //Other urls left out as assumed to be working since source url is working
         assertContains(
-            curseForgeMod.authors,
-            CurseForgeResponse.ModResponse.ModAuthor(
-                302655,
-                "jellysquid3_",
-                "https://www.curseforge.com/members/28746583-jellysquid3_?username=jellysquid3_"
+            curseForgeMod.authors, CurseForgeResponse.ModResponse.ModAuthor(
+                452596, "NebelNidas", "https://www.curseforge.com/members/100230862-nebelnidas?username=nebelnidas"
             )
         )
 
-        assertEquals(emptyList(), curseForgeApiCall.files(394468, CurseForgeApiCall.ModLoaderType.FORGE)!!.data)
+        assertEquals(
+            emptyList(), curseForgeApiCall.files(
+                curseApiKey(), 533960, modLoaderType = CurseForgeApiCall.ModLoaderType.CAULDRON.curseNumber
+            ).execute().body()!!.data
+        )
 
-        val sodiumFiles = curseForgeApiCall.files(394468)
-        assertNotNull(sodiumFiles)
+        val curseForgeFiles = curseForgeApiCall.files(curseApiKey(), 533960).execute().body()
+        assertNotNull(curseForgeFiles)
 
-        assertContains(
-            sodiumFiles.data,
+        assertEquals(
             CurseForgeResponse.FileResponse(
-                3669187,
+                3481563,
                 true,
-                "Sodium mc1.18.2-0.4.1",
-                "https://edge.forgecdn.net/files/3669/187/sodium-fabric-mc1.18.2-0.4.1+build.15.jar",
-                listOf("Fabric", "1.18.2")
-            )
+                "Modget 0.1.0",
+                "https://edge.forgecdn.net/files/3481/563/modget-0.1.0.jar",
+                listOf("Fabric", "1.16.5", "1.16.4")
+            ), curseForgeFiles.data.last()
         )
 
         //We did not specify max files, so we should get all files
-        assertEquals(sodiumFiles.pagination.resultCount, sodiumFiles.pagination.totalCount)
+        assertTrue { curseForgeFiles.pagination.resultCount == curseForgeFiles.pagination.totalCount }
     }
 
     /**
-     * Uses Sodium by jellysquid to test the API call.
+     * Uses Modget by ReviversMC to test the API call.
      *
      * @author ReviversMC
      * @since 1.0.0
@@ -92,31 +84,32 @@ class ApiCallTest : KoinTest {
     fun `modrinth api test`() {
         val modrinthApiCall by inject<ModrinthApiCall>()
 
-        //Lowercase all the fields we can for reliability. Those not lowercase are from case sensitive fields (e.g. id)
-        val sodiumProject = modrinthApiCall.project("AANobbMI")
-        assertNotNull(sodiumProject)
-        assertEquals("AANobbMI", sodiumProject.id)
-        assertEquals("sodium", sodiumProject.title.lowercase())
-        assertEquals("lgpl-3", sodiumProject.license!!.id.lowercase())
-        assertEquals("https://github.com/caffeinemc/sodium-fabric", sodiumProject.sourceUrl!!.lowercase())
-        //Other urls left out as assumed to be working since source url is working
-        assertContains(sodiumProject.versions, "74Y5Z8fo") //This version is v0.4.1
+        //Lowercase all the fields we can for reliability. Those not lowercase are from case-sensitive fields (e.g. id)
+        val modrinthProject = modrinthApiCall.project("2NpFE0R3").execute().body()
+        assertNotNull(modrinthProject)
+        assertEquals("2NpFE0R3", modrinthProject.id)
+        assertEquals("modget", modrinthProject.title.lowercase())
+        assertEquals("lgpl-3", modrinthProject.license!!.id.lowercase())
+        assertEquals("https://github.com/reviversmc/modget", modrinthProject.sourceUrl!!.lowercase())
 
-        val sodiumVersion010 = modrinthApiCall.versions("AANobbMI").last()
-        assertNotNull(sodiumVersion010)
-        assertEquals("Sodium 0.1.0", sodiumVersion010.name)
-        assertEquals(listOf("1.16.5", "1.16.4", "1.16.3"), sodiumVersion010.gameVersions.sortedDescending())
-        assertEquals(listOf("fabric"), sodiumVersion010.loaders.map { it.lowercase() })
-        assertContains(
-            sodiumVersion010.files,
+        val modgetVersion001 = modrinthApiCall.versions("2NpFE0R3").execute().body()!!.last()
+        assertNotNull(modgetVersion001)
+        assertEquals("Modget 0.0.1", modgetVersion001.name)
+        assertEquals(listOf("1.16.5", "1.16.4"), modgetVersion001.gameVersions.sortedDescending())
+        assertEquals(listOf("fabric"), modgetVersion001.loaders.map { it.lowercase() })
+        assertEquals(
             ModrinthResponse.VersionResponse.VersionFile(
-                ModrinthResponse.VersionResponse.VersionFile.VersionHash("2597064f7116315adcfe114f0d76c04233791fed049cbbcf722f878002bf14cd8dec421806da639f31ffd496b9dedac9e39e2b48f43007427fa0f659ec5118e2"),
-                "https://cdn.modrinth.com/data/AANobbMI/versions/mc1.16.3-0.1.0/sodium-fabric-mc1.16.3-0.1.0.jar",
-                true
-            )
+                ModrinthResponse.VersionResponse.VersionFile.VersionHash("7dc82b00a305d8793a6787897f6c3bcf415e75ed0a257f17c40046320a1f3b686a1195eb3d4a3c36acd9b41308819315c2eb804194e44f5fe7fa303e5afc4fbc"),
+                "https://cdn.modrinth.com/data/2NpFE0R3/versions/0.0.1/modget-0.0.1.jar",
+            ), modgetVersion001.files.last()
         )
 
-        assertEquals("jellysquid3", modrinthApiCall.projectOwner("AANobbMI")!!.lowercase())
+        assertEquals(
+            "NebelNidas",
+            modrinthApiCall.projectMembers("2NpFE0R3").execute().body()
+                ?.first { member -> member.role == "Owner" }?.userResponse?.username
+                ?: throw IOException("No owner found for modrinth project: 2NpFE0R3")
+        )
     }
 
 
