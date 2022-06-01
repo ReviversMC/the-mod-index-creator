2cfc2a7c977697dac0ca8e6223b03a2cc8154276
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-05-31 18:41:31 -0700

    Add annotation to cli main

--- cli/src/main/kotlin/com/github/reviversmc/themodindex/creator/cli/Cli.kt
+++ cli/src/main/kotlin/com/github/reviversmc/themodindex/creator/cli/Cli.kt
@@ -2,16 +2,16 @@ package com.github.reviversmc.themodindex.creator.cli
 
 import com.github.reviversmc.themodindex.creator.core.apicalls.apiCallModule
 import com.github.reviversmc.themodindex.creator.core.creatorModule
-import com.github.reviversmc.themodindex.creator.core.dependency.dependencyModule
+import kotlinx.serialization.ExperimentalSerializationApi
 import org.koin.core.context.startKoin
 
 
+@ExperimentalSerializationApi
 fun main() {
     startKoin {
         modules(
             apiCallModule,
-            creatorModule,
-            dependencyModule
+            creatorModule
         )
     }
 }
