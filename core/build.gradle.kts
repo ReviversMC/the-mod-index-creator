ebacf3a62180afeb61d76f1041d3059a59dbc14d
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-05-31 19:36:13 -0700

    Update index api used

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,6 +3,6 @@ plugins {
 }
 
 dependencies {
-    api("com.github.reviversmc:the-mod-index-api:5.1.0")
+    api("com.github.reviversmc:the-mod-index-api:5.1.2")
     api("org.kohsuke:github-api:1.306")
 }
