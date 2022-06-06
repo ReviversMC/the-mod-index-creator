bdbbd312c8334eb5c3355f92e853bc7cce39928f
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-06-05 18:24:36 -0700

    More apis and define main class

--- githubapp/build.gradle.kts
+++ githubapp/build.gradle.kts
@@ -1,3 +1,14 @@
 dependencies {
     api(project(":core"))
+    api("com.squareup.okhttp3:okhttp:4.9.3")
+    api("io.fusionauth:fusionauth-jwt:5.2.0")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "${rootProject.group}.ghapp.GitHubAppKt"
+        }
+    }
 }
