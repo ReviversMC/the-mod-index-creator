9118fb1649d466d2f6c5207a65d724395e1b76db
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-06-14 13:08:57 -0700

    Remove unnecessary module

--- cli/src/main/kotlin/com/github/reviversmc/themodindex/creator/cli/Cli.kt
+++ cli/src/main/kotlin/com/github/reviversmc/themodindex/creator/cli/Cli.kt
@@ -1,16 +1,12 @@
 package com.github.reviversmc.themodindex.creator.cli
 
-import com.github.reviversmc.themodindex.creator.core.apicalls.apiCallModule
 import com.github.reviversmc.themodindex.creator.core.creatorModule
-import kotlinx.serialization.ExperimentalSerializationApi
 import org.koin.core.context.startKoin
 
 
-@ExperimentalSerializationApi
 fun main() {
     startKoin {
         modules(
-            apiCallModule,
             creatorModule
         )
     }
