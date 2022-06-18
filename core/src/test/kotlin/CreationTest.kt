30380b2f53c9ff8396bfd6d73cbee1997ac2631d
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-06-17 22:19:15 -0700

    TODO Filter CF downloadable file by path params

--- core/src/test/kotlin/CreationTest.kt
+++ core/src/test/kotlin/CreationTest.kt
@@ -1,7 +1,8 @@
 import com.github.reviversmc.themodindex.creator.core.Creator
-import com.github.reviversmc.themodindex.creator.core.apicalls.ModrinthProjectResponse
+import com.github.reviversmc.themodindex.creator.core.data.ManifestWithGenericIdentifier
+import com.github.reviversmc.themodindex.creator.core.data.ThirdPartyApiUsage
+import io.mockk.mockkClass
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import okhttp3.mockwebserver.Dispatcher
 import okhttp3.mockwebserver.MockResponse
@@ -13,88 +14,120 @@ import org.koin.core.parameter.parametersOf
 import org.koin.test.KoinTest
 import org.koin.test.get
 import org.koin.test.junit5.KoinTestExtension
+import org.koin.test.junit5.mock.MockProviderExtension
 import java.io.File
 import java.util.*
+
 import kotlin.test.assertEquals
 
 class CreationTest : KoinTest {
 
     // TODO Mock GitHub API, and finish fake CF server
-
+    /*
+    We CANNOT decode (json) string inputs to a Kotlin object, as that would remove unrecognized fields.
+    We want the json with unrecognized fields to be sent as an api response.
+    */
     private val baseUrl = "http://localhost" // Ensure not https!
+    private val curseForgeModId = 463481
+    private val modrinthProjectId = "2NpFE0R3"
 
-    private val modrinthProjectResponse =
-        this.javaClass.getResource("/TMIConsumer/modrinth/modrinthProject.json")?.readText()
+    private val curseForgeModResponse = readResource("/ApiResponse/CurseForge/curseMod.json")
+    private val curseForgeFilesResponse = readResource("/ApiResponse/CurseForge/curseFiles.json")
 
-    private val modrinthTeamResponse = this.javaClass.getResource("/TMIConsumer/modrinth/modrinthTeam.json")?.readText()
+    private val modrinthProjectResponse = readResource("/ApiResponse/Modrinth/modrinthProject.json")
+    private val modrinthTeamResponse = readResource("/ApiResponse/Modrinth/modrinthTeam.json")
+    private val modrinthVersionResponse = readResource("/ApiResponse/Modrinth/modrinthVersions.json")
 
-    private val modrinthVersionResponse =
-        this.javaClass.getResource("/TMIConsumer/modrinth/modrinthVersions.json")?.readText()
+    private var isCurseForgeDistribution = true
+    private var isTestingForGitHub = false
 
-    @Suppress("JSON_FORMAT_REDUNDANT") // We only use this instance once, and all other instances will be dependency injected.
-    private val decodedModrinthProjectResponse =
-        modrinthProjectResponse?.let { Json { ignoreUnknownKeys = true }.decodeFromString<ModrinthProjectResponse>(it) }
-
-    private val projectId = decodedModrinthProjectResponse?.id ?: "0"
-    private val versionId = decodedModrinthProjectResponse?.versions?.first() ?: "0"
 
     private val curseForgeServer = MockWebServer().apply {
         dispatcher = object : Dispatcher() {
             override fun dispatch(request: RecordedRequest): MockResponse {
                 if (request.getHeader("x-api-key") == null) return MockResponse().setResponseCode(401)
 
-                return MockResponse().setResponseCode(500)
-            }
-        }
-    }
+                return when (request.path?.substringBefore("?")) {
+                    "/v1/mods/$curseForgeModId" -> MockResponse().setResponseCode(200)
+                        .setBody(if (isTestingForGitHub) curseForgeModResponse else buildString {
+                            curseForgeModResponse.split("\n").forEach {
+                                if (it.contains("\"sourceUrl\":")) {
+                                    append("${it.substringBefore("\"sourceUrl\":")}\"sourceUrl\": null\n")
+                                } else append("$it\n")
+                            }
+                        })
 
-    private var isTestingForGitHub = false
 
-    private val modrinthServer = MockWebServer().apply {
-
-        dispatcher = object : Dispatcher() {
-
-            override fun dispatch(request: RecordedRequest): MockResponse {
-                return when (request.path) {
-                    "/v2/project/$projectId" -> modrinthProjectResponse?.let {
-                        MockResponse().setResponseCode(
-                            200
-                        ).setBody(
-                            if (isTestingForGitHub) it else get<Json>().encodeToString(
-                                decodedModrinthProjectResponse!!.copy( // Shouldn't be null as this was decoded from modrinthProjectResponse
-                                    sourceUrl = null
+                    "/v1/mods/$curseForgeModId/files" -> MockResponse().setResponseCode(200).setBody(buildString {
+                        curseForgeFilesResponse.split("\n").forEach {
+                            if (it.contains("\"downloadUrl\"")) {
+                                if (isCurseForgeDistribution) append(
+                                    it.replace(
+                                        "{port}", downloadServer.port.toString()
+                                    )
                                 )
-                            )
-                        )
-                    } ?: MockResponse().setResponseCode(500)
+                                else append("${it.substringBefore("\"downloadUrl\"")}\"downloadUrl\": null,\n")
+                            } else append("$it\n")
+                        }
+                    })
 
-                    "/v2/project/$projectId/members" -> modrinthTeamResponse?.let {
-                        MockResponse().setResponseCode(
-                            200
-                        ).setBody(it)
-                    } ?: MockResponse().setResponseCode(500)
-
-                    "/v2/project/$projectId/version" -> modrinthVersionResponse?.let {
-                        MockResponse().setResponseCode(
-                            200
-                        ).setBody(it)
-                    } ?: MockResponse().setResponseCode(500)
-
-                    else -> MockResponse().setBody(request.path.toString()).setResponseCode(404)
+                    else -> MockResponse().setResponseCode(404).setBody(request.path.toString())
                 }
             }
         }
     }
 
+    private val modrinthServer = MockWebServer().apply {
+        dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return when (request.path?.substringBefore("?")) {
+                    "/v2/project/$modrinthProjectId" -> MockResponse().setResponseCode(200)
+                        .setBody(if (isTestingForGitHub) modrinthProjectResponse else buildString {
+                            modrinthProjectResponse.split("\n").forEach {
+                                if (it.contains("\"source_url\":")) {
+                                    append("${it.substringBefore("\"source_url\":")}\"source_url\": null,\n")
+                                } else append("$it\n")
+                            }
+                        })
+
+                    "/v2/project/$modrinthProjectId/members" -> MockResponse().setResponseCode(200)
+                        .setBody(modrinthTeamResponse)
+                    "/v2/project/$modrinthProjectId/version" -> MockResponse().setResponseCode(200)
+                        .setBody(modrinthVersionResponse)
+                    else -> MockResponse().setResponseCode(404).setBody(request.path.toString())
+                }
+            }
+        }
+    }
+
+    private val downloadServer = MockWebServer().apply {
+        dispatcher = object : Dispatcher() {
+            override fun dispatch(request: RecordedRequest): MockResponse {
+                return if (request.path?.startsWith("http://localhost/files/") == true) {
+                    MockResponse().setResponseCode(200).setBody(
+                        readResource(
+                            "/ApiResponse/DownloadableFiles/${request.path!!.removePrefix("http://localhost/files/")}"
+                        )
+                    )
+                } else MockResponse().setResponseCode(404).setBody(request.path.toString())
+            }
+        }
+    }
+
+    private fun readResource(path: String) =
+        javaClass.getResource(path)?.readText() ?: throw NoSuchFileException(file = File(path))
+
     @BeforeEach
     fun `before each`() {
         curseForgeServer.start()
+        downloadServer.start()
         modrinthServer.start()
     }
 
     @AfterEach
     fun `after each`() {
         curseForgeServer.shutdown()
+        downloadServer.shutdown()
         modrinthServer.shutdown()
     }
 
@@ -102,10 +135,14 @@ class CreationTest : KoinTest {
     @RegisterExtension
     val koinTestExtension = KoinTestExtension.create { modules(fakeCreatorModule) }
 
+    @JvmField
+    @RegisterExtension
+    val mockProvider = MockProviderExtension.create { mockkClass(it) }
+
     private fun gitHubOAuthToken(): String {
         val properties = Properties()
         properties.load(
-            this.javaClass.getResourceAsStream("/credentials.properties") ?: return System.getenv("GITHUB_OAUTH")
+            javaClass.getResourceAsStream("/credentials.properties") ?: return System.getenv("GITHUB_OAUTH")
                 ?: "broken-api-key"
         )
         return properties.getProperty("github_oAuth") ?: System.getenv("GITHUB_OAUTH") ?: "broken-api-key"
@@ -122,15 +159,35 @@ class CreationTest : KoinTest {
 
         val json = get<Json>()
 
-        isTestingForGitHub = false.also {
-            assertEquals( //Test for a pure modrinth config, without CurseForge or GitHub.
+        /**
+         * Checks if there is the same manifest between the file in resources and the one generated by the creator.
+         * [testType] needs to be specified to determine which manifest to compare to (e.g. "pureModrinth, "pureCurseForge")
+         */
+        fun assertManifestEquals(testType: String, manifestWithGenericIdentifier: ManifestWithGenericIdentifier) {
+            assertEquals(
                 json.decodeFromString(
-                    javaClass.getResource("/TMIConsumer/expected_manifests/pureModrinth.json")?.readText()
-                        ?: throw NoSuchFileException(file = File("/TMIConsumer/expected_manifests/pureModrinth.json")),
-                ), creator.createManifestModrinth(projectId).manifestsWithIdentifiers.first().manifestJson
+                    readResource(
+                        "/ExpectedManifest/$testType/${
+                            manifestWithGenericIdentifier.genericIdentifier.substringBefore(
+                                ":"
+                            )
+                        }/${manifestWithGenericIdentifier.genericIdentifier.substringAfter(":")}.json"
+                    )
+                ), manifestWithGenericIdentifier.manifestJson
             )
         }
 
+        isTestingForGitHub = false.apply {
+            isCurseForgeDistribution = true.apply {
+                val pureCurseForge = creator.createManifestCurseForge(curseForgeModId)
+                assertEquals(pureCurseForge.thirdPartyApiUsage, listOf(ThirdPartyApiUsage.CURSEFORGE_USED))
+                pureCurseForge.manifestsWithGenericIdentifiers.forEach { assertManifestEquals("pureCurseForge", it) }
+            }
+
+            val pureModrinth = creator.createManifestModrinth(modrinthProjectId)
+            assertEquals(pureModrinth.thirdPartyApiUsage, listOf(ThirdPartyApiUsage.MODRINTH_USED))
+            pureModrinth.manifestsWithGenericIdentifiers.forEach { assertManifestEquals("pureModrinth", it) }
+        }
 
 
     }
