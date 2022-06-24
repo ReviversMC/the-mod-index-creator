e5a0a13f394f489ed708d1cfc7e1058176e3c09f
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-06-24 13:14:20 -0700

    Add a logger :D

--- githubapp/build.gradle.kts
+++ githubapp/build.gradle.kts
@@ -1,8 +1,13 @@
 dependencies {
+    // Core dependencies
     api(project(":core"))
-    api("com.squareup.okhttp3:okhttp:4.9.3")
     api("io.fusionauth:fusionauth-jwt:5.2.0")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+    api("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.4")
+    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.3")
+
+    // Loggers
+    api("ch.qos.logback:logback-classic:1.2.11")
+    api("io.github.microutils:kotlin-logging-jvm:2.1.23")
 }
 
 tasks {
