b5fdd62c987e4ee7f9f164dfc058ba3161b3e1f9
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-06-24 17:20:02 -0700

    Create app config

---
+++ githubapp/src/main/kotlin/com/github/reviversmc/themodindex/creator/ghapp/data/AppConfig.kt
@@ -0,0 +1,10 @@
+package com.github.reviversmc.themodindex.creator.ghapp.data
+
+@kotlinx.serialization.Serializable
+data class AppConfig(
+    val discordBotToken: String,
+    val discordChannel: Long,
+    val discordServer: Long,
+    val gitHubAppId: String,
+    val gitHubPrivateKeyPath: String,
+)
