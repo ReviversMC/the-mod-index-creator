61021083fb9affdc41b229a9a0972f34ecae824a
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-03 16:31:06 -0700

    Specify type of Discord channel

--- maintainer/src/main/kotlin/com/github/reviversmc/themodindex/creator/ghapp/data/AppConfig.kt
+++ maintainer/src/main/kotlin/com/github/reviversmc/themodindex/creator/ghapp/data/AppConfig.kt
@@ -3,8 +3,8 @@ package com.github.reviversmc.themodindex.creator.ghapp.data
 @kotlinx.serialization.Serializable
 data class AppConfig(
     val discordBotToken: String,
-    val discordChannel: Long,
     val discordServer: Long,
+    val discordTextChannel: Long,
     val gitHubAppId: String,
     val gitHubPrivateKeyPath: String,
     val gitHubRepoOwner: String,
