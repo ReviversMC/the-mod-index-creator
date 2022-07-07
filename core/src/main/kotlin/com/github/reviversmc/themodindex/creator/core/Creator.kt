e4b0e158fddbf6687088d5757e6b96d93d6c5e75
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-06 19:14:30 -0700

    Add illegal state exception into kdoc

--- core/src/main/kotlin/com/github/reviversmc/themodindex/creator/core/Creator.kt
+++ core/src/main/kotlin/com/github/reviversmc/themodindex/creator/core/Creator.kt
@@ -9,6 +9,7 @@ interface Creator {
      * Creates a [ManifestWithApiStatus], which contains [ManifestJson]s
      * When used in Kotlin, [curseForgeId] cannot be null, while [modrinthId] can be null.
      * When there is a conflict in data, CurseForge data will be preferred if [preferCurseForgeData] is true.
+     * @throws IllegalArgumentException If the wrong index version is targeted.
      * @author ReviversMC
      * @since 1.0.0
      */
@@ -22,6 +23,7 @@ interface Creator {
      * Creates a [ManifestWithApiStatus], which contains [ManifestJson]s
      * When used in Kotlin, [modrinthId] cannot be null, while [curseForgeId] can be null.
      * When there is a conflict in data, Modrinth data will be preferred if [preferModrinthData] is true.
+     * @throws IllegalArgumentException If the wrong index version is targeted.
      * @author ReviversMC
      * @since 1.0.0
      */
