1b1ef2d372959fe2095e6962b965ad2b9f40069d
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-22 10:59:13 -0700

    Update dependencies/move to stable versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "7.1.2"
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
 }
 
 group = "com.github.reviversmc.themodindex.creator"
