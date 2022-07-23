7a1ddcf88a61ea45f73be6f79128b1fc27c0867c
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-22 22:08:41 -0700

    Add more variables to pass in DI

--- maintainer/src/main/kotlin/com/github/reviversmc/themodindex/creator/ghapp/github/UpdateSenderModule.kt
+++ maintainer/src/main/kotlin/com/github/reviversmc/themodindex/creator/ghapp/github/UpdateSenderModule.kt
@@ -1,20 +1,27 @@
 package com.github.reviversmc.themodindex.creator.ghapp.github
 
 import com.github.reviversmc.themodindex.creator.core.dependency.dependencyModule
+import org.koin.core.parameter.parametersOf
+import org.koin.core.qualifier.named
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val updateSenderModule = module {
 
-    factory { (repoOwner: String, repoName: String, targetedBranch: String, gitHubAppId: String, gitHubPrivateKeyPath: String) ->
+    /*
+    These are the below referenced variables:
+    (manifestRepo: String?, repoOwner: String, repoName: String, targetedBranch: String, gitHubAppId: String, gitHubPrivateKeyPath: String)
+    */
+
+    factory {
         GitHubUpdateSender(
+            it.get<String?>(0)?.let { manifestRepo -> get(named("custom")) { parametersOf(manifestRepo) } } ?: get(),
             get(),
-            get(),
-            repoOwner,
-            repoName,
-            targetedBranch,
-            gitHubAppId,
-            gitHubPrivateKeyPath
+            it[1],
+            it[2],
+            it[3],
+            it[4],
+            it[5]
         )
     } bind UpdateSender::class
     includes(dependencyModule)
