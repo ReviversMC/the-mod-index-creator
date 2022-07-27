2945dab7f037788cb6d21a6be6b0827fefa4e127
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-26 20:49:02 -0700

    Debug options when boot

--- maintainer/src/main/kotlin/com/github/reviversmc/themodindex/creator/maintainer/Maintainer.kt
+++ maintainer/src/main/kotlin/com/github/reviversmc/themodindex/creator/maintainer/Maintainer.kt
@@ -102,23 +102,23 @@ fun main(args: Array<String>) = runBlocking {
 
     val configLocation by commandParser.option(
         ArgType.String, shortName = "c", description = "The location of the config file"
-    ).default("the-mod-index-maintainer/config.json")
+    ).default("the-mod-index-maintainer/config.json").also { logger.debug { "Config location set to $it" } }
 
     val cooldownInHours by commandParser.option(
         ArgType.Int, shortName = "d", description = "How long to delay between updates"
-    ).default(12)
+    ).default(12).also { logger.debug { "Cooldown set to $it hours" } }
 
     val sus by commandParser.option(
         ArgType.Boolean,
         shortName = "s",
         description = "Whether to be suspicious of all updates, and push to a separate branch for PR review"
-    ).default(false)
+    ).default(false).also { logger.debug { "Sus mode: $it" } }
 
     val testMode by commandParser.option(
         ArgType.Boolean,
         shortName = "t",
         description = "Whether to be in test mode, and push to the maintainer-test branch"
-    ).default(false)
+    ).default(false).also { logger.debug("Test mode: $it") }
 
 
     commandParser.parse(args)
