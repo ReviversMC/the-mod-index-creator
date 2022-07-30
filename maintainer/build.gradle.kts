57fd31b332550d2b90f779b10921eaa76129aa57
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-29 21:08:05 -0700

    Update kotlin cli

--- maintainer/build.gradle.kts
+++ maintainer/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     api("dev.kord:kord-core:0.8.0-M15")
     api("io.fusionauth:fusionauth-jwt:5.2.0")
     api("io.github.java-diff-utils:java-diff-utils:4.11")
-    api("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.4")
+    api("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.5")
 
     // Loggers
     api("ch.qos.logback:logback-classic:1.2.11")
