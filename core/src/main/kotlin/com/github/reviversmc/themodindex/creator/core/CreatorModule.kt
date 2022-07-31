03b70384c4c98af92b7b898ca3a83f1df30816c2
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-07-30 23:09:15 -0700

    Make Creator single so that we don't create multiple scheduled repeating tasks

--- core/src/main/kotlin/com/github/reviversmc/themodindex/creator/core/CreatorModule.kt
+++ core/src/main/kotlin/com/github/reviversmc/themodindex/creator/core/CreatorModule.kt
@@ -7,7 +7,7 @@ import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val creatorModule = module {
-    factory { (curseForgeApiKey: String, refreshGitHubClient: () -> ApolloClient) ->
+    single { (curseForgeApiKey: String, refreshGitHubClient: () -> ApolloClient) ->
         ModIndexCreator(
             get(), curseForgeApiKey, get(), refreshGitHubClient, get(), get()
         )
