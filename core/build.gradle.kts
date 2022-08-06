d94c9f718b991493b14c2f2d38ac552b1b85985e
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-08-06 16:45:44 -0700

    Update apollo

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.apollographql.apollo3").version("3.3.2")
+    id("com.apollographql.apollo3").version("3.4.0")
 }
 
 dependencies {
-    api("com.apollographql.apollo3:apollo-runtime:3.3.2")
+    api("com.apollographql.apollo3:apollo-runtime:3.4.0")
     api("com.github.reviversmc:the-mod-index-api:9.0.0")
 }
 
