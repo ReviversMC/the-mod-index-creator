4b7d4fee7dd008c302245853aaaf91eed29a81e2
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2022-08-06 17:18:27 -0700

    Add toml parser

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,7 +2,12 @@ plugins {
     id("com.apollographql.apollo3").version("3.4.0")
 }
 
+repositories {
+    maven("https://jitpack.io")
+}
+
 dependencies {
+    api("cc.ekblad:4koma:1.1.0")
     api("com.apollographql.apollo3:apollo-runtime:3.4.0")
     api("com.github.reviversmc:the-mod-index-api:9.0.0")
 }
